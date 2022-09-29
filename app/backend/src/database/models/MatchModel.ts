69ed4d84b75ae31c6eaadb5f6f8a5a419a95aba8
Andre Rousselet <andre098@hotmail.com>
2022-09-29 13:11:06 -0300

    feat(match model): add MatchModel
    provide association 1:N with TeamModel

---
+++ app/backend/src/database/models/MatchModel.ts
@@ -0,0 +1,54 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import TeamModel from './TeamModel';
+
+class MatchModel extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+}
+
+MatchModel.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+MatchModel.belongsTo(TeamModel, { foreignKey: 'homeTeam', as: 'teamHome' });
+MatchModel.belongsTo(TeamModel, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+TeamModel.hasMany(MatchModel, { foreignKey: 'homeTeam', as: 'match' });
+TeamModel.hasMany(MatchModel, { foreignKey: 'awayTeam', as: 'match' });
+
+export default MatchModel;
