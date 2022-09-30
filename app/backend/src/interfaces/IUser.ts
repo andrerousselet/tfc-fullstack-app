8c7a8f734550680da625f4dea3c03e03b206a4fd
Andre Rousselet <andre098@hotmail.com>
2022-09-30 20:30:27 -0300

    feat(user interface): add IUser interface extending from ILogin
    (also rename file)

---
+++ app/backend/src/interfaces/IUser.ts
@@ -0,0 +1,12 @@
+import { Role } from '../database/models/User';
+
+export interface ILogin {
+  email: string;
+  password: string;
+}
+
+export interface IUser extends ILogin {
+  id: number;
+  username: string;
+  role: Role;
+}
