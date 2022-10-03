31c778c0564ee22154624121c42ff25bf086a52c
Andre Rousselet <andre098@hotmail.com>
2022-10-03 15:33:15 -0300

    feat(error middleware): change from ifs to switch case
    add one more check of error instance (json web token)

--- app/backend/src/middlewares/handleError.ts
+++ app/backend/src/middlewares/handleError.ts
@@ -1,13 +1,19 @@
 import { ErrorRequestHandler } from 'express';
+import { JsonWebTokenError } from 'jsonwebtoken';
 import CustomError from '../CustomError';
 import StatusCodes from '../StatusCodes';
 
 const handleError: ErrorRequestHandler = (error, _req, res, _next) => {
   console.log(error);
-  if (error instanceof CustomError) {
-    return res.status(error.statusCode).json({ message: error.message });
+  switch (true) {
+    case error instanceof CustomError:
+      return res.status(error.statusCode).json({ message: error.message });
+    case error instanceof JsonWebTokenError:
+      return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Invalid token' });
+    default:
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ message: 'Something went wrong...' });
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Something went wrong...' });
 };
 
 export default handleError;
