44a32c2eff61235eaa00ca39ed7442a576c163c8
Andre Rousselet <andre098@hotmail.com>
2022-10-03 14:35:44 -0300

    feat(user service): add static method to generate token and verify valid user info
    check user email and password using compareSync (bycriptjs), returning token if valid info and
    throwing custom error if not

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -1,6 +1,8 @@
 import * as Joi from 'joi';
+import * as jwt from 'jsonwebtoken';
+import { compareSync } from 'bcryptjs';
 import UserModel from '../models/UserModel';
-import { ILogin } from '../interfaces/IUser';
+import { ILogin, IUser } from '../interfaces/IUser';
 import CustomError from '../CustomError';
 import StatusCodes from '../StatusCodes';
 
@@ -10,16 +12,25 @@ export default class UserService {
   async login(user: ILogin) {
     UserService.validateLogin(user);
     const foundUser = await this.userModel.findUser(user.email);
-    if (!foundUser) throw new Error('usuário não encontrado');
-    return '123456789123456789';
+    const validPass = compareSync(user.password, foundUser?.password as string);
+    if (!foundUser || !validPass) {
+      throw new CustomError(StatusCodes.UNAUTHORIZED, 'Incorrect email or password');
+    }
+    const token = UserService.generateToken(foundUser);
+    return token;
   }
 
   static validateLogin(user: ILogin) {
     const loginSchema = Joi.object({
       email: Joi.string().email().required(),
-      password: Joi.string().required(),
+      password: Joi.string().min(6).required(),
     });
     const { error } = loginSchema.validate(user);
     if (error) throw new CustomError(StatusCodes.BAD_REQUEST, 'All fields must be filled');
+  }
+
+  static generateToken(user: IUser) {
+    const token = jwt.sign({ data: user }, process.env.JWT_SECRET as string);
+    return token;
   }
 }
