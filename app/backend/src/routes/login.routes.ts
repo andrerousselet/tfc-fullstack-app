d77c2425ac10367f1df8c870884063bb127a4e5c
Andre Rousselet <andre098@hotmail.com>
2022-10-03 15:35:56 -0300

    feat(login routes): add GET /login/validate endpoint

--- app/backend/src/routes/login.routes.ts
+++ app/backend/src/routes/login.routes.ts
@@ -6,5 +6,6 @@ const userController = new UserController();
 const router = Router();
 
 router.post('/login', (req, res, next) => userController.login(req, res, next));
+router.get('/login/validate', (req, res, next) => userController.validateToken(req, res, next));
 
 export default router;
