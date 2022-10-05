fb5b9bfdec5505e07b43a96166fd14e9aecf421e
Andre Rousselet <andre098@hotmail.com>
2022-10-05 14:18:53 -0300

    feat(team service): add findById method in TeamService

--- app/backend/src/services/TeamService.ts
+++ app/backend/src/services/TeamService.ts
@@ -1,3 +1,5 @@
+import StatusCodes from '../StatusCodes';
+import CustomError from '../CustomError';
 import TeamModel from '../models/TeamModel';
 
 export default class TeamService {
@@ -6,5 +8,13 @@ export default class TeamService {
   async findAll() {
     const teams = await this.teamModel.findAll();
     return teams;
+  }
+
+  async findById(id: number) {
+    const team = await this.teamModel.findById(id);
+    if (!team) {
+      throw new CustomError(StatusCodes.NOT_FOUND, 'Team doesn`t exist');
+    }
+    return team;
   }
 }
