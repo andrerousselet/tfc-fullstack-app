65b5209998ca123e91e0e7ffcd16b9f67ac09369
Andre Rousselet <andre098@hotmail.com>
2022-10-05 14:20:06 -0300

    feat(teams routes): add GET /teams/:id endpoint in teams.routes

--- app/backend/src/routes/teams.routes.ts
+++ app/backend/src/routes/teams.routes.ts
@@ -6,5 +6,6 @@ const teamController = new TeamController();
 const router = Router();
 
 router.get('/teams', (req, res, next) => teamController.findAll(req, res, next));
+router.get('/teams/:id', (req, res, next) => teamController.findById(req, res, next));
 
 export default router;
