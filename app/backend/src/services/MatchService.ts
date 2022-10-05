f1194a098371eb5a36067b8d1314d67e4c68ff94
Andre Rousselet <andre098@hotmail.com>
2022-10-05 19:26:25 -0300

    feat(match service): add finishMatch method checking if match exists

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -1,6 +1,8 @@
 import InProgress from '../types/InProgress';
 import MatchModel from '../models/MatchModel';
 import { IMatch } from '../interfaces/IMatch';
+import CustomError from '../helpers/CustomError';
+import StatusCodes from '../helpers/StatusCodes';
 
 export default class MatchService {
   constructor(private matchModel = new MatchModel()) {}
@@ -17,5 +19,11 @@ export default class MatchService {
   async create(match: IMatch) {
     const createdMatch = await this.matchModel.create(match);
     return createdMatch;
+  }
+
+  async finishMatch(id: number) {
+    const match = await this.matchModel.findById(id);
+    if (!match) throw new CustomError(StatusCodes.NOT_FOUND, 'Match doesn`t exist');
+    await this.matchModel.finishMatch(id);
   }
 }
