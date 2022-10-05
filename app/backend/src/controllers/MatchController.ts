2584d69cb274cbc790107fb939638b820a986e07
Andre Rousselet <andre098@hotmail.com>
2022-10-05 18:26:35 -0300

    feat(match controller): add create method

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -1,16 +1,31 @@
 import { RequestHandler } from 'express';
+import { IMatch } from '../interfaces/IMatch';
 import InProgress from '../types/InProgress';
 import StatusCodes from '../helpers/StatusCodes';
 import MatchService from '../services/MatchService';
+import UserService from '../services/UserService';
 
 export default class MatchController {
-  constructor(private matchService = new MatchService()) {}
+  constructor(
+    private matchService = new MatchService(),
+    private userService = new UserService(),
+  ) {}
 
   findAll: RequestHandler = async (req, res, next) => {
     try {
       const { inProgress } = req.query;
       const matches = await this.matchService.findAll(inProgress as InProgress);
       return res.status(StatusCodes.OK).json(matches);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  create: RequestHandler = async (req, res, next) => {
+    try {
+      const match = req.body as IMatch;
+      const createdMatch = await this.matchService.create(match);
+      return res.status(StatusCodes.CREATED).json(createdMatch);
     } catch (error) {
       next(error);
     }
