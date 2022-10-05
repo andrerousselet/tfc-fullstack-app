ca6043d170dffc65c9a6be0447ed74ad8c523b1d
Andre Rousselet <andre098@hotmail.com>
2022-10-05 18:27:00 -0300

    feat(routes): add POST /matches endpoint

--- app/backend/src/routes/matches.routes.ts
+++ app/backend/src/routes/matches.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import MatchController from '../controllers/MatchController';
+import auth from '../middlewares/auth';
 
 const matchController = new MatchController();
 
 const router = Router();
 
 router.get('/matches', (req, res, next) => matchController.findAll(req, res, next));
+router.post('/matches', auth, (req, res, next) => matchController.create(req, res, next));
 
 export default router;
