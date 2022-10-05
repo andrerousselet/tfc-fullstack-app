5553084bc0b2e50bed5c4e10bee7e44a71193fb9
Andre Rousselet <andre098@hotmail.com>
2022-10-05 16:16:37 -0300

    feat(match service): return all matches or filtered matches depending on inProgress param

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -1,10 +1,15 @@
+import InProgress from '../types/InProgress';
 import MatchModel from '../models/MatchModel';
 
 export default class MatchService {
   constructor(private matchModel = new MatchModel()) {}
 
-  async findAll() {
+  async findAll(inProgress: InProgress) {
     const matches = await this.matchModel.findAll();
+    if (inProgress === 'true' || inProgress === 'false') {
+      const matchesByProgress = matches.filter((match) => String(match.inProgress) === inProgress);
+      return matchesByProgress;
+    }
     return matches;
   }
 }
