a92974682890fd80505f42f1a53e7e25331e6487
Andre Rousselet <andre098@hotmail.com>
2022-10-05 15:10:37 -0300

    fix(model): fix aliases hasMany associations between Team and Match

--- app/backend/src/database/models/Match.ts
+++ app/backend/src/database/models/Match.ts
@@ -48,7 +48,7 @@ Match.init({
 Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'teamHome' });
 Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'teamAway' });
 
-Team.hasMany(Match, { foreignKey: 'homeTeam', as: 'match' });
-Team.hasMany(Match, { foreignKey: 'awayTeam', as: 'match' });
+Team.hasMany(Match, { foreignKey: 'homeTeam', as: 'teamHome' });
+Team.hasMany(Match, { foreignKey: 'awayTeam', as: 'teamAway' });
 
 export default Match;
