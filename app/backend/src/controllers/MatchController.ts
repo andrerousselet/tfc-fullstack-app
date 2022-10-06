661d75f0630662caf9b0ea445de83db4bb63ef68
Andre Rousselet <andre098@hotmail.com>
2022-10-06 10:27:35 -0300

    feat(match controller): add updateScores method returning updatedMatch as response

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from 'express';
-import { IMatch } from '../interfaces/IMatch';
+import { IMatch, IMatchGoals } from '../interfaces/IMatch';
 import InProgress from '../types/InProgress';
 import StatusCodes from '../helpers/StatusCodes';
 import MatchService from '../services/MatchService';
@@ -32,6 +32,19 @@ export default class MatchController {
       const { id } = req.params;
       await this.matchService.finishMatch(Number(id));
       return res.status(StatusCodes.OK).json({ message: 'Finished' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  updateScores: RequestHandler = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const updatedScores = req.body as IMatchGoals;
+      const updatedMatch = await this.matchService.updateScores(
+        { id: Number(id), ...updatedScores },
+      );
+      return res.status(StatusCodes.OK).json(updatedMatch);
     } catch (error) {
       next(error);
     }
