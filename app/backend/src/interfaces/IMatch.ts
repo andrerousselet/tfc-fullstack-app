9c0cc1d3fec6bf4289552625f329a590ffb74ff1
Andre Rousselet <andre098@hotmail.com>
2022-10-09 16:30:07 -0300

    feat(interfaces): add IMatchStats interface
    also refactor IMatch and compose it with ITeamName

--- app/backend/src/interfaces/IMatch.ts
+++ app/backend/src/interfaces/IMatch.ts
@@ -7,8 +7,24 @@ export interface IUpdateScores extends IMatchGoals {
   id?: number;
 }
 
+export interface ITeamName {
+  teamName: string;
+}
+
 export interface IMatch extends IUpdateScores {
   homeTeam: number;
   awayTeam: number;
   inProgress: boolean;
+  teamHome?: ITeamName;
+  teamAway?: ITeamName;
+}
+
+export interface IMatchStats {
+  name: string;
+  totalPoints: number;
+  totalVictories: number;
+  totalLosses: number;
+  totalDraws: number;
+  goalsFavor: number;
+  goalsOwn: number;
 }
